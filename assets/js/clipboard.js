320a55dce0f60a381671308f52fe3bf8e852ba7b
Mateo Alexandru <161512990+amethystmcnet@users.noreply.github.com>
2024-08-02 00:54:29 +0200

    dawd

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('play.amethystmc.ovh')
+    swal({
+    title: 'IP COPIATO',
+    text: 'Buon divertimento',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
